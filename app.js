6656d345b85099316ef7db3d8e408e623d91f8a4
Jeroen Rinzema <ik@jeroenrinzema.nl>
2015-11-18 11:53:17 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,134 @@
+var express = require('express'),
+		app = express(),
+		server = require('http').createServer(app);
+
+var bodyParser = require('body-parser'),
+		ical = require('ical-generator'),
+		cal = ical(),
+		moment = require('moment'),
+		request = require("request");
+
+//Settings
+settings = {
+	port: 4000,
+	api: 'https://xvilo.com/cibap/json.php',
+	jaar: moment().format('YYYY'),
+	week: moment().week(),
+	triggerAlert: 600
+}
+
+lesuuren = {
+	"0":{"start": {"hour": 8, "minute": 45}, "end": {"hour": 9, "minute": 15}},
+	"1":{"start": {"hour": 9, "minute": 15}, "end": {"hour": 9, "minute": 45}},
+	"2":{"start": {"hour": 9, "minute": 45}, "end": {"hour": 10, "minute": 15}},
+	"3":{"start": {"hour": 10, "minute": 30}, "end": {"hour": 11, "minute": 00}},
+	"4":{"start": {"hour": 11, "minute": 00}, "end": {"hour": 11, "minute": 30}},
+	"5":{"start": {"hour": 11, "minute": 30}, "end": {"hour": 12, "minute": 00}},
+	"6":{"start": {"hour": 12, "minute": 00}, "end": {"hour": 12, "minute": 30}},
+	"7":{"start": {"hour": 12, "minute": 30}, "end": {"hour": 13, "minute": 00}},
+	"8":{"start": {"hour": 13, "minute": 00}, "end": {"hour": 13, "minute": 30}},
+	"9":{"start": {"hour": 13, "minute": 30}, "end": {"hour": 14, "minute": 00}},
+	"10":{"start": {"hour": 14, "minute": 00}, "end": {"hour": 14, "minute": 30}},
+	"11":{"start": {"hour": 14, "minute": 45}, "end": {"hour": 15, "minute": 15}},
+	"12":{"start": {"hour": 15, "minute": 15}, "end": {"hour": 15, "minute": 45}},
+	"13":{"start": {"hour": 15, "minute": 45}, "end": {"hour": 16, "minute": 15}},
+	"14":{"start": {"hour": 16, "minute": 15}, "end": {"hour": 16, "minute": 45}},
+	"15":{"start": {"hour": 16, "minute": 45}, "end": {"hour": 17, "minute": 15}},
+	"16":{"start": {"hour": 16, "minute": 45}, "end": {"hour": 17, "minute": 15}}
+};
+
+//Core
+cibap = {
+	init: function() {
+		app.use(bodyParser.json());
+
+		app.all('*', function(req, res, next) {
+		    res.header("Access-Control-Allow-Origin", "*");
+  			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, TokenId, userid");
+  			next();
+		});
+
+		server.listen(settings.port);
+		console.log('Server is running on port: '+settings.port);
+
+		app.get('/:soort/:type', cibap.ical);
+
+		var event = ical().createEvent();
+	},
+	ical: function(req, res, next) {
+		//Genereer rooster
+		cal.setDomain('cibaptothetop.nl').setName('Cibap: '+req.params.type);
+		var host = settings.api+'?klas='+req.params.type+'&soort='+req.params.soort+'&week='+settings.week+'&jaar='+settings.jaar;
+
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+		request(host, function(error, response, body) {
+			if(error || !body) {
+				res.send(error);
+				next();
+				return console.log(error);
+			}
+
+			if (!/^[\],:{}\s]*$/.test(body.replace(/\\["\\\/bfnrtu]/g, '@').
+			replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+			replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+				return res.send(false);
+			}
+
+			var body = JSON.parse(body);
+
+			for (i = 0; i < 5; i++) {
+				var dag = i+1,
+					old = '';
+				if(!body[dag]) {
+					return;
+				}
+
+				body[dag].forEach(function(uur, key){
+					if(key == 15) {
+						create(old, key);
+					} else {
+						if(old == '') {
+							old = lesuuren[key].start;
+						}
+
+						if(uur[0] != body[dag][key+1][0]) {
+							create(old, key);
+							old = '';
+						}
+					}
+
+					function create(start, close) {
+						var startLes = moment(start).weekday(dag);
+						var endLes = moment(lesuuren[close].end).weekday(dag);
+						if(uur[0]) {
+							cal.addEvent({
+							    start: startLes._d,
+							    end: endLes._d,
+							    summary: uur[0],
+							    description: uur[2],
+							    location: uur[1]
+							});
+						}
+					}
+				});
+
+				if(i == 4) {
+					var events = cal.events();
+
+					events.forEach(function(event) {
+						event.createAlarm({
+						    type: 'audio',
+						    trigger: settings.triggerAlert, // 5min before event
+						});
+					});
+
+					cal.serve(res);
+					cal.clear();
+					next();
+				}
+			}
+		});
+	}
+};
+
+cibap.init();
